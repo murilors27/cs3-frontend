cd19b2bce9b1729ff23bbf7751381f065ea16c41
murilors27 <murilorsantos27@gmail.com>
2024-09-11 17:16:31 -0300

    modificando o container

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import "./container.css"
+import Header from '../header'
+import Footer from '../Footer'
 
 type ContainerProps = {
     children: React.ReactNode,
@@ -8,7 +10,11 @@ type ContainerProps = {
 
 const Container: React.FC <ContainerProps> = ({children, img}) => {
   return (
-    <div style={{ backgroundImage: `url(/assets/${img})` }} className="container">{children}</div>
+    <div style={{ backgroundImage: `url(/assets/${img})` }} className="container">
+      <Header></Header>
+      {children}
+      <Footer></Footer>
+      </div>
   )
 }
 
